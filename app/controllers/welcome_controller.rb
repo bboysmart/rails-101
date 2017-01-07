73077a97479b91a48d18aa7dcdf78a8ef5448112
wangzhihui <wangzhihui2017@gmail.com>
2017-01-07 17:36:14 +0800

    create groups index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,16 @@
 class WelcomeController < ApplicationController
+  def indew
+    flash[:notice] ="青春！美好！"
+  end
 end
+class WelcomeController <ApplicationController
+  def indew
+    flash[:alert] ="夕阳！美好！"
+  end
+  end
+class WelcomeController <ApplicationController
+  def indew
+    flash[:warning] = "来自 smart 消息"
+  end
+
+  end
